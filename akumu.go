0e2781c6ede32d6633db6e075584c339018fa824
Èrik C. Forés <soc@erik.cat>
2023-12-09 00:52:58 +0100

    feat: routing

--- akumu.go
+++ akumu.go
@@ -4,7 +4,6 @@ import (
 	netHttp "net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/studiolambda/akumu/http"
 )
 
 type Akumu struct {
@@ -15,10 +14,6 @@ func New() *Akumu {
 	return &Akumu{
 		router: chi.NewRouter(),
 	}
-}
-
-func (akumu *Akumu) Get(path string, handler http.Handler) {
-	akumu.router.Method(string(http.MethodGet), path, handler)
 }
 
 func (akumu *Akumu) Start() {
---
+++ routing.go
@@ -0,0 +1,43 @@
+package akumu
+
+import "github.com/studiolambda/akumu/http"
+
+func (akumu *Akumu) Handle(method http.Method, path string, handler http.Handler) {
+	akumu.router.Method(string(method), path, handler)
+}
+
+func (akumu *Akumu) Get(path string, handler http.Handler) {
+	akumu.Handle(http.MethodGet, path, handler)
+}
+
+func (akumu *Akumu) Post(path string, handler http.Handler) {
+	akumu.Handle(http.MethodPost, path, handler)
+}
+
+func (akumu *Akumu) Put(path string, handler http.Handler) {
+	akumu.Handle(http.MethodPut, path, handler)
+}
+
+func (akumu *Akumu) Patch(path string, handler http.Handler) {
+	akumu.Handle(http.MethodPatch, path, handler)
+}
+
+func (akumu *Akumu) Delete(path string, handler http.Handler) {
+	akumu.Handle(http.MethodDelete, path, handler)
+}
+
+func (akumu *Akumu) Head(path string, handler http.Handler) {
+	akumu.Handle(http.MethodHead, path, handler)
+}
+
+func (akumu *Akumu) Connect(path string, handler http.Handler) {
+	akumu.Handle(http.MethodConnect, path, handler)
+}
+
+func (akumu *Akumu) Options(path string, handler http.Handler) {
+	akumu.Handle(http.MethodOptions, path, handler)
+}
+
+func (akumu *Akumu) Trace(path string, handler http.Handler) {
+	akumu.Handle(http.MethodTrace, path, handler)
+}
