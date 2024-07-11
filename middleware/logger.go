22d2846d25b4315cd6b58ea17d53a6f25771fcc5
Èrik C. Forés <soc@erik.cat>
2024-07-11 15:45:13 +0200

    feat: better logger

--- middleware/logger.go
+++ middleware/logger.go
@@ -8,7 +8,19 @@ import (
 	"github.com/studiolambda/akumu"
 )
 
-func Logger(handler http.Handler, logger *slog.Logger) http.Handler {
+func Logger(logger *slog.Logger) func(http.Handler) http.Handler {
+	return func(handler http.Handler) http.Handler {
+		return LoggerWith(handler, logger)
+	}
+}
+
+func LoggerDefault() func(http.Handler) http.Handler {
+	return func(handler http.Handler) http.Handler {
+		return LoggerWith(handler, slog.Default())
+	}
+}
+
+func LoggerWith(handler http.Handler, logger *slog.Logger) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		handler.ServeHTTP(writer, request.WithContext(
 			context.WithValue(request.Context(), akumu.LoggerKey{}, logger),
